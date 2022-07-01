b32d2e714de53773d42c2981ba4cb4e5b139ff12
Amzad hossain <amzad.rafi@northsouth.edu>
2022-07-01 23:36:07 +0600

    utils improved for annotation

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@ import shutil
 import keras
 import os
 import yaml
+import numpy as np
 
+images, targets = [], []
 path_images = "/101_ObjectCategories/airplanes/"
 path_annot = "/Annotations/Airplanes_Side_2/"
 
@@ -23,9 +25,7 @@ def Download_data():
     )
 
     # Extracting tar files found inside main zip file
-    shutil.unpack_archive("/datasets/caltech-101/101_ObjectCategories.tar.gz", "/")
-    shutil.unpack_archive("/datasets/caltech-101/Annotations.tar", "/")
-
+    
     shutil.unpack_archive("/datasets/caltech-101/101_ObjectCategories.tar.gz", "/")
     shutil.unpack_archive("/datasets/caltech-101/Annotations.tar", "/")
 
@@ -50,5 +50,42 @@ def read_yaml(path='config.yaml'):
         data_loaded = yaml.load(stream, Loader=yaml.FullLoader)
     return data_loaded
 
+for i in range(0, len(annot_paths)):
+    # Access bounding box coordinates
+    annot = scipy.io.loadmat(path_annot + annot_paths[i])["box_coord"][0]
+
+    top_left_x, top_left_y = annot[2], annot[0]
+    bottom_right_x, bottom_right_y = annot[3], annot[1]
+
+    image = keras.utils.load_img(
+        path_images + image_paths[i],
+    )
+    (w, h) = image.size[:2]
+
+    # resize train set images
+    if i < int(len(annot_paths) * 0.8):
+        # resize image if it is for training dataset
+        image = image.resize((image_size, image_size))
+
+    # convert image to array and append to list
+    images.append(keras.utils.img_to_array(image))
+
+    # apply relative scaling to bounding boxes as per given image and append to list
+    targets.append(
+        (
+            float(top_left_x) / w,
+            float(top_left_y) / h,
+            float(bottom_right_x) / w,
+            float(bottom_right_y) / h,
+        )
+    )
+(x_train), (y_train) = (
+    np.asarray(images[: int(len(images) * 0.8)]),
+    np.asarray(targets[: int(len(targets) * 0.8)]),
+)
+(x_test), (y_test) = (
+    np.asarray(images[int(len(images) * 0.8) :]),
+    np.asarray(targets[int(len(targets) * 0.8) :]),
+)
 
 config= read_yaml()
