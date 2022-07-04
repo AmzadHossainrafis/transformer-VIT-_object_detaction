0bc65e2fa37231dfde13e0bb40742262ca9d8c47
Amzad hossain <amzad.rafi@northsouth.edu>
2022-07-05 04:05:50 +0600

    model with config implemented

--- model.py
+++ model.py
@@ -1,38 +1,36 @@
 from custom_layers import Patches, PatchEncoder, mlp
 from tensorflow.keras import layers
 from tensorflow import keras
-from utils import read_yaml
-config = read_yaml("config.yaml")
+from utils import config
 
 
+num_patches = (config["image_size"] // config["patch_size"]) ** 2
 
-def Vit_model(config,
-    input_shape,patch_size,num_patches,projection_dim,num_heads,transformer_units,transformer_layers,mlp_head_units,
-):
+def Vit_model(config=config):
     """
     
     retruns: Vit model 
     """
-    inputs = layers.Input(shape=input_shape)
+    inputs = layers.Input(shape=config["input_shape"])
     # Create patches
-    patches = Patches(patch_size)(inputs)
+    patches = Patches(config["patch_size"])(inputs)
     # Encode patches
-    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+    encoded_patches = PatchEncoder(num_patches, config["projection_dim"])(patches)
 
     # Create multiple layers of the Transformer block.
-    for _ in range(transformer_layers):
+    for _ in range(config["transformer_layers"]):
         # Layer normalization 1.
         x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
         # Create a multi-head attention layer.
         attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+            num_heads=config["num_heads"], key_dim=config["projection_dim"], dropout=0.1
         )(x1, x1)
         # Skip connection 1.
         x2 = layers.Add()([attention_output, encoded_patches])
         # Layer normalization 2.
         x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
         # MLP
-        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        x3 = mlp(x3, hidden_units=config["transformer_units"], dropout_rate=0.1)
         # Skip connection 2.
         encoded_patches = layers.Add()([x3, x2])
 
@@ -41,7 +39,7 @@ def Vit_model(config,
     representation = layers.Flatten()(representation)
     representation = layers.Dropout(0.3)(representation)
     # Add MLP.
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.3)
+    features = mlp(representation, hidden_units=config['mlp_head_units'], dropout_rate=0.3)
 
     bounding_box = layers.Dense(4)(
         features
@@ -50,3 +48,5 @@ def Vit_model(config,
     # return Keras model.
     return keras.Model(inputs=inputs, outputs=bounding_box)
 
+model=Vit_model()
+model.summary()
