49884b6bf5ca8a00f7f2c40f2202a169ed4f0279
Amzad hossain <amzad.rafi@northsouth.edu>
2022-07-05 00:45:53 +0600

    bug fixed : chg all img to formet units8

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,9 @@
-from utils import  config 
+from utils import  config ,data_preproses
 import tensorflow as tf 
 from tensorflow.keras.utils import Sequence
 import math
+import numpy as np
+
 
 class Dataloader(Sequence):
     def __init__(self,batch_size,train_data,labels):
@@ -15,13 +17,24 @@ class Dataloader(Sequence):
     def __getitem__(self, index):
         img_batch=self.train_data[index*self.batch_size:(index+1)*self.batch_size]
         label_batch=self.labels[index*self.batch_size:(index+1)*self.batch_size]
-
-        return img_batch,label_batch
-        
+        # x = np.zeros((self.batch_size,config['image_size'],config['image_size'],3))
+        # y = np.zeros((self.batch_size) + 4, dtype="float32")
+        x,y=[],[]
+        for i in range(self.batch_size):
+            x.append(img_batch[i])
+            y.append(label_batch[i] )
+        #return img_batch ,label_batch 
+        return tf.convert_to_tensor(x),tf.convert_to_tensor(y) 
 
         
         
 if __name__ == '__main__':
-    dataloader=Dataloader(batch_size=2,train_data,labels)
-    print(dataloader[0].shape())
-    print(dataloader[1].shape())
+    x_train, y_train, x_test, y_test= data_preproses()
+    dataloader=Dataloader(batch_size=256,train_data=x_train,labels=y_train)
+    data=dataloader[0]
+    print(data[0].shape)
+    print(data[1].shape)
+    #print(len(dataloader[1]))
+
+# import matplotlib.pyplot as plt
+# plt.imshow(data[0][0])
